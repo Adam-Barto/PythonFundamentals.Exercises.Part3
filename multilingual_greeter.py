aa46c6e0c01e145434c000d3da11ada8cf49cbfa
Adam-Barto <adam.jbarto@gmail.com>
2023-03-15 11:17:55 -0400

    Used Tuples to complete the Language Greeter

--- multilingual_greeter.py
+++ multilingual_greeter.py
@@ -5,18 +5,21 @@ def greet(lang, name):
 def language_input():
     s = input('1: English\n2: Spanish\n3: French\n')
     if s == '1':
-        return 'Hello '
+        return 'Hello ', 'What\'s your name? '
     elif s == '2':
-        return 'Hola '
+        return 'Hola ', '¿Cómo te llamas'
     elif s == '3':
-        return 'Bonjour '
+        return 'Bonjour ', 'Comment tu t\'appelles '
     else:
-        return 'Howdy'
+        return 'Howdy', 'What\'s your name partner? '
+
+
+question = language_input()
 
 
 def name_input():
-    name = input('Please provide a name ')
+    name = input(question[1])
     return name
 
 
-greet(language_input(), name_input())
+greet(question[0], name_input())
